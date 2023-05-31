3f8e42546ecabd798b97f93670bdc67e2d93fc81
bhargavi <bhargavikandula220@gmail.com>
2023-05-31 14:55:39 +0530

    first repository

---
+++ AdditionOfNum/src/com/sathya/Addition.java
@@ -0,0 +1,15 @@
+package com.sathya;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int n=10;
+		int n1=20;
+		int res=n+n1;
+		System.out.println("addition of 2 numbers :" + res);
+
+	}
+
+}
